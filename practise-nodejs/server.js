7b545b13290cae9b4e223e6aa75a9f4a220cd1ae
calmesam01 <sanyam@shairyourcar.com>
2023-01-19 14:43:31 -0800

    Practise

--- practise-nodejs/server.js
+++ practise-nodejs/server.js
@@ -1,7 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    res.setHeader("content-type", "text/html")
+    res.write("<h1>Hello World!</h1>");
+    res.end()
 })
 
 const listenToServer = server.listen(3000, () => {
